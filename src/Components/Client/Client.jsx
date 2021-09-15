64d9f39dcf554bf4546607ff4bae5d828883b603
Mehedi-Developer <mehedihasan30313@gmail.com>
2021-09-16 00:25:51 +0600

    Something updated

--- src/Components/Client/Client.jsx
+++ src/Components/Client/Client.jsx
@@ -1,30 +1,6 @@
 import React from 'react';
 
 const Client = () => {
-    // let n = 10;
-    // n = "Word";
-    // let name = "hasan";
-    // const obj1 = {
-    //     name,
-    //     dept: "CSE",
-    //     id: 1,
-    //     sum: (a,b) => (a+b),
-    // }
-    // console.log({sum: obj1?.sum(5, 6)});
-    // const array = [1,3,5,4,5,6,7,8];
-    // call, bind , apply
-    // console.log(...array);
-    // for (let index = 0; index < array.length; index++) {
-    //     const element = array[index];
-    //     console.log({element});
-    // }
-    // map, find, filter
-    // array?.map(function(element) { return console.log({element});});
-    // // let a, b;
-    // const [a,b, ...rest] = array;
-    // console.log({a}, {b}, rest);
-    // const odd = array?.find(e => (e % 2));
-    // console.log({odd});
     return (
         <div>
             <h1 className="text-center text-info mt-5">Client UI</h1>
